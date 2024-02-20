540a001d84204cdb3da001fff53c4a9374b5b4f6
StormBreaker2022 <114773085+StormBreaker2022@users.noreply.github.com>
2024-02-20 19:20:42 +0530

    addded functionality for getting remaining donation target

--- backend/model/individualPayment.js
+++ backend/model/individualPayment.js
@@ -1,9 +1,10 @@
+import { Int32 } from 'mongodb';
 import mongoose from 'mongoose';
 
 const PaymentSchema = mongoose.Schema({
     title: { type: String, required: true },
     number: {type:Number, requireed: true},
-    budget: {type: String, required: true},
+    budget: {type: Number, required: true},
     projectID: { type: String, required: true },
     paymentID: { type: String, required: true },
     payerID: { type: String, required: true },
--- backend/model/payment.js
+++ backend/model/payment.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const PaymentSchema = mongoose.Schema({
     title: { type: String, required: true },
     number: {type:Number, requireed: true},
-    budget: {type: String, required: true},
+    budget: {type: Number, required: true},
     projectID: { type: String, required: true },
     paymentID: { type: String, required: true },
     payerID: { type: String, required: true },
