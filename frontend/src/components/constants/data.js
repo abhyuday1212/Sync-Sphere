aab150581c9a5439b5f371da89f11007e847c020
Akhileshmalik23 <akhileshmalik908@gmail.com>
2024-02-20 22:25:33 +0530

    add FAQS

--- frontend/src/components/constants/data.js
+++ frontend/src/components/constants/data.js
@@ -7,3 +7,26 @@ export const catagories = [
     { id: 5, type: "Cleaniness" },
     { id: 6, type: "Greenify" },
 ]
+export const faqs = [
+    { id: 1, type: "How does SyncSphere simplify corporate social responsibility (CSR) for companies?", discription: "SyncSphere offers a curated list of societal issues, streamlining the process of finding suitable CSR projects for companies. It also provides access to CSR documents directly from NGOs, enabling efficient evaluation and decision-making." },
+    { id: 2, type: "How does SyncSphere facilitate communication between stakeholders", discription: "SyncSphere enables direct, real-time conversations between major CSR companies, donors, and NGOs through its chat feature. This fosters transparency and constructive dialogue, leading to impactful partnerships and collaborations." },
+    {
+        id: 3, type: "How can I list a project on SyncSphere?", discription: `To list a project on SyncSphere, simply follow these steps:
+    Click on the "Create Project" button located on the platform's interface.
+    Choose the category or societal issue for which you want to create the project.
+    Follow the prompts to provide details about your project, including its objectives, requirements, and expected outcomes.
+    Once you have entered all the necessary information, submit the project for listing on SyncSphere.` },
+    {
+        id: 4, type: "How do I use the chat app on SyncSphere?", discription: `Navigate to the "Project" section of SyncSphere.
+    Locate the project where you want to contribute.
+    Click on the "JOIN" button associated with the project.
+    Upon joining, you will be directed to a page where the chat app interface is accessible.
+    Engage in real-time conversations with other stakeholders involved in the project, including volunteers, corporations, and NGOs.` },
+    {
+        id: 5, type: "Is SyncSphere a free platform?", discription: `Free Features: Users can create projects on SyncSphere for free. If you're seeking contributions of less than 5000 rupees, you can utilize these free features to list your project and connect with volunteers, corporations, and NGOs without incurring any charges.
+
+        Paid Features: While basic project listing is free, SyncSphere also offers paid options for users who require additional features or seek contributions exceeding 5000 rupees. Users can choose from various paid plans tailored to their needs for enhanced visibility, advanced project management tools, and other premium features.
+        
+        By providing both free and paid options, SyncSphere ensures accessibility for users with different requirements and budgets, empowering them to make meaningful contributions towards societal issues. ` },
+
+]
